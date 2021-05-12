d438b275e6884cedd8cee0e1a1b936d7bd183a76
Shivam Balwani <shivambalwani0407@gmail.com>
2021-05-12 15:34:58 +0530

    Settings edited

--- frontida_backend/settings.py
+++ frontida_backend/settings.py
@@ -143,9 +143,9 @@ DATABASES = {
             "PORT": "5432",
         }
     }
-if IS_PRODUCTION:
-    DATABASES["default"] = dj_database_url.config()
-    DATABASES["default"]["ENGINE"] = "django.contrib.gis.db.backends.postgis"
+
+DATABASES["default"] = dj_database_url.config()
+DATABASES["default"]["ENGINE"] = "django.contrib.gis.db.backends.postgis"
 
 
 
