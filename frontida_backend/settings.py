e9338a29ddae6a7cfac26e174a7dae3db6b33f80
Krishna Gaur <gaurkrishna498@gmail.com>
2021-05-14 08:18:14 +0530

    Resolved config error in deployment

--- frontida_backend/settings.py
+++ frontida_backend/settings.py
@@ -210,7 +210,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+# EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 # EMAIL_HOST_USER = "healthcare.frontida@gmail.com"
 # EMAIL_HOST_PASSWORD = "iicnoaupdzdaqgmf"
 
