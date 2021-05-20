de63a4061736fc5ff74e166d544fe1f25bd6d1f5
Krishna Gaur <gaurkrishna498@gmail.com>
2021-05-20 09:21:23 +0530

    black formatting applied

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,11 +7,21 @@ from .views import *
 
 
 urlpatterns = [
-    path("user_verification/<uidb64>/<token>", UserVerification, name="user_verification"),
+    path(
+        "user_verification/<uidb64>/<token>", UserVerification, name="user_verification"
+    ),
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginAPI.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("user-details/", UserDetailsView.as_view(), name="user_details"),
-    path("request-reset-email/", RequestPasswordResetEmail.as_view(), name="request-reset-email"),
-    path("password-reset-confirm/<uidb64>/<token>/", PasswordResetConfirm.as_view(), name="password-reset-confirm"),
+    path(
+        "request-reset-email/",
+        RequestPasswordResetEmail.as_view(),
+        name="request-reset-email",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        PasswordResetConfirm.as_view(),
+        name="password-reset-confirm",
+    ),
 ]
--- medical_store/utils.py
+++ medical_store/utils.py
@@ -1,10 +1,12 @@
 from .models import MedicineInventory
 import datetime
+
+
 class CheckExpiry:
-    def check(self,user):
-        data=MedicineInventory.objects.filter(account=user)
-        sys_date=datetime.date.today()
-        expired_medicine=[]
+    def check(self, user):
+        data = MedicineInventory.objects.filter(account=user)
+        sys_date = datetime.date.today()
+        expired_medicine = []
         for medicine in data:
             if (medicine.expiry - sys_date) < datetime.timedelta(days=90):
                 medicine.isexpired = True
