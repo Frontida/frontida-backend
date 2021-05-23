657372a8112cd2e051f43513a88b6ad58e0064f0
Shivam Balwani <201851122@iiitvadodara.ac.in>
2021-05-23 08:56:43 +0530

    Signin Tests

--- authentication/test_modules/authentication_tests.py
+++ authentication/test_modules/authentication_tests.py
@@ -1,1 +1,30 @@
 from django.test import TestCase
+from authentication.models import User
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate
+import uuid
+from unittest import mock
+from authentication.utils import Utils
+from authentication.views import LoginAPI
+
+
+class SigninTests(TestCase):
+    def setUp(self):
+        self.list_view = LoginAPI.as_view()
+        self.user_1 = User.objects.create(
+            email="test_1@gmail.com", password="test1234", is_verified=True
+        )
+        self.user_2 = User.objects.create(email="test_2@gmail.com", password="test1234")
+        self.factory = APIRequestFactory()
+
+    def test_not_verified_user(self):
+        request_url = "/login/"
+        create_request = self.factory.post(
+            request_url,
+            data={"email": self.user_2.email, "password": "test1234"},
+            format="json",
+        )
+        response = self.list_view(
+            create_request,
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
