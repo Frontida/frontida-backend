decb7d17a421a5901268885be93fe3e5d2c6c38a
Krishna Gaur <gaurkrishna498@gmail.com>
2021-06-05 21:33:39 +0530

    divided serializers in individual modules

---
+++ authentication/Serializers/loginserializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=255)
+    password = serializers.CharField(max_length=68, min_length=8)
+
+    class Meta:
+        fields = ["email", "password"]
---
+++ authentication/Serializers/passwordresetemail.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+class PasswordResetEmailRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+
+    class Meta:
+        fields = ["email"]
