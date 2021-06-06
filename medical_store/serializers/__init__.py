d489b7f96683e60e1a028d7cf42f3da0f76b00ee
Krishna Gaur <gaurkrishna498@gmail.com>
2021-06-06 15:58:17 +0530

    modularized serializers

---
+++ medical_store/serializers/__init__.py
@@ -0,0 +1,6 @@
+from .companydetailsserializer import *
+from .medicineinventoryserializer import *
+from .purchaseinventoryserializer import *
+from .purchaseserializer import *
+from .salesinventoryserializers import *
+from .salesserializer import *
---
+++ medical_store/serializers/salesinventoryserializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from ..models import SalesInventory
+
+class SalesInventorySerializers(ModelSerializer):
+    class Meta:
+        model = SalesInventory
+        exclude = ["sales_id", "isexpired"]
