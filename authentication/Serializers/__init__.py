f09c67038308ba128b7234fae856e6f38d415695
Krishna Gaur <gaurkrishna498@gmail.com>
2021-06-06 15:48:27 +0530

    added __init__ files and its imports

---
+++ authentication/Serializers/__init__.py
@@ -0,0 +1,5 @@
+from .loginserializer import *
+from .passwordresetemail import *
+from .registerserializer import *
+from .setnewpasswordserializer import *
+from .userdetailsserializer import *
---
+++ authentication/Views/__init__.py
@@ -0,0 +1,7 @@
+from .loginview import *
+from .logoutview import *
+from .passwordresetemailview import *
+from .passwordresetconfirm import *
+from .registerview import *
+from .userdetailsview import *
+from .userverificationview import *
